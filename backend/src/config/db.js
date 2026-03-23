25ca7058a81903517ad688fad1f690f1c5fc9e98
Assuman <assumanmugisha773@gmail.com>
2026-03-23 08:26:17 -0700

    updates

---
+++ backend/src/config/db.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2/promise');
+
+const pool =mysql.createPool({
+    host : process.env.DB_HOST,
+    port : process.env.DB_PORT,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
+
+});
+
+pool.getConnection()
+            .then(connection =>{
+                console.log('database connected successfully');
+                connection.release();
+            })
+
+.catch(err=>{
+    consol.log('error is connected successfuly!');
+
+});
+
+module.export = pool;
