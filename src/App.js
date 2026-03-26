7ecf1ec01f0cf1ec78c770597a79ddc14467b32e
Assuman <assumanmugisha773@gmail.com>
2026-03-26 04:03:21 -0700

    backend patient

---
+++ src/App.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const  express= require('express');
+const PORT = process.env.PORT;
+const patientRoutes = require('./routes/patients');
+const app = express();
+app.use(express.json());
+app.use('/api/patients', patientRoutes);
+
+const db = require('./config/db');
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+module.exports = app;
